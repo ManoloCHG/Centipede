c587e5674ab19c1238bee8ef539df875f5d39ee9
ManoloCHG <46519969+ManoloCHG@users.noreply.github.com>
2019-02-27 10:26:49 +0100

    Merge origin/master
    Conflicts:
    	src/com/pradopolixe/centipede/Main.java

--- src/com/pradopolixe/centipede/model/Centipede.java
+++ src/com/pradopolixe/centipede/model/Centipede.java
@@ -19,18 +19,14 @@ public class Centipede {
     }
     
     public void mostrarTablero() {
-        //Mostrar la matriz
+        //Mostrar la matriz y enemigo 
         for(int y=0; y<20; y++) {
             for(int x=0; x<20; x++) {
-                if (matrizTablero[x][y] == matrizTablero[xEnemigo][yEnemigo]){
-                    matrizTablero [x][y] = '2';
-                     System.out.print(matrizTablero[x][y]);
+                if (x == xEnemigo && y == yEnemigo){
+                    System.out.print("3");
                 }else{
-                    if (matrizTablero[x][y] != matrizTablero[xEnemigo][yEnemigo]){
-                        System.out.print(matrizTablero[x][y]);
-            }
-          }
-                
+                  System.out.print(matrizTablero[x][y]);
+                }    
             }
             System.out.println();
         }       
@@ -41,46 +37,54 @@ public class Centipede {
         Random rnd = new Random();
         for (int i=0; i<20; i++){
         int x = rnd.nextInt(20);
-        int y = rnd.nextInt(20);
+        int y = rnd.nextInt(18);
         while (matrizTablero[x][y] == '5'){
             x = rnd.nextInt(20);
-            y = rnd.nextInt(20);
+            y = rnd.nextInt(18);
         } 
         matrizTablero[x][y] = '5';
         }
     }
-   
-    //Colocar enemigo
-//    public void colocarEnemigo(){
-//        matrizTablero[xEnemigo][yEnemigo] = '2';
-//    }
     //Mover enemigo
     int xEnemigo = 1;
     int yEnemigo = 0;
     int velocidadCienpies = 1;
     public void moverEnemigo(){
+        int xFutura = xEnemigo + velocidadCienpies;
         if (xEnemigo == 0){
-            //matrizTablero[xEnemigo][yEnemigo] = '0';
             yEnemigo ++;
-            //matrizTablero[xEnemigo][yEnemigo] = '0';
             velocidadCienpies = 1;
             xEnemigo += velocidadCienpies;
-            //this.colocarEnemigo();
         }else{
             if(xEnemigo == 19){
-              //matrizTablero[xEnemigo][yEnemigo] = '0';
               yEnemigo++;
-              //matrizTablero[xEnemigo][yEnemigo] = '0';
               velocidadCienpies = -1;
               xEnemigo += velocidadCienpies;
-              //this.colocarEnemigo();
             }else{
-            if (xEnemigo != 0 && xEnemigo != 19){
-                //matrizTablero[xEnemigo][yEnemigo] = '0';
+                if(matrizTablero[xFutura][yEnemigo] == '5' && velocidadCienpies == 1){
+                yEnemigo++;
+                velocidadCienpies = -1;
                 xEnemigo += velocidadCienpies;
-                //this.colocarEnemigo();
+                }else{
+                    if (matrizTablero[xFutura][yEnemigo] == '5' && velocidadCienpies == -1){
+                        yEnemigo ++;
+                        velocidadCienpies = 1;
+                        xEnemigo += velocidadCienpies;
+                    }else{
+                        if (xEnemigo != 0 && xEnemigo != 19){
+                        xEnemigo += velocidadCienpies;
+                        }
+                    }
+                }
             }
-          }
+        }
+    }
+    public void moverNave(int xOrigen) {  
+        try {
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("ERROR: No existe la posición indicada");
+        } catch(Exception e) {
+            System.out.println("ERROR desconocido");
         }
     }
 }
